b369f316ace297dcd0099f662d7b6dcc56fc9fad
MattMin <mzyupc@163.com>
2019-11-06 09:50:19 +0800

    添加注释

--- parent-demo/order-demo/src/main/java/com/mzyupc/order/service/impl/fallback/OrderFeignFallBackServiceImpl.java
+++ parent-demo/order-demo/src/main/java/com/mzyupc/order/service/impl/fallback/OrderFeignFallBackServiceImpl.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Service;
 /**
  * @author mzyupc@163.com
  * @date 2019/11/5 10:10:46
+ *
+ * 通过feign接口远程调用的fallback方法
  */
 @Service
 public class OrderFeignFallBackServiceImpl implements OrderFeignService {
