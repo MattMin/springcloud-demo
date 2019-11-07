e5f96354936aee1357b102060a47139852420db5
MattMin <mzyupc@163.com>
2019-11-07 14:49:05 +0800

    分布式配置中心高可用集群

--- parent-demo/config-client-demo/src/main/java/com/mzyupc/config/ConfigClientApp.java
+++ parent-demo/config-client-demo/src/main/java/com/mzyupc/config/ConfigClientApp.java
@@ -2,12 +2,14 @@ package com.mzyupc.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author mzyupc@163.com
  * @date 2019/11/6 16:27:35
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class ConfigClientApp {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApp.class, args);
--- parent-demo/config-server-demo/src/main/java/com/mzyupc/config/ConfigServerApp.java
+++ parent-demo/config-server-demo/src/main/java/com/mzyupc/config/ConfigServerApp.java
@@ -3,6 +3,7 @@ package com.mzyupc.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author mzyupc@163.com
@@ -10,6 +11,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableConfigServer
+@EnableEurekaClient
 public class ConfigServerApp {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApp.class, args);
