8d91ff7c5d92c700b6fc27544e9d646be3d5b98b
MattMin <mzyupc@163.com>
2019-11-07 13:49:21 +0800

    config+bug实现动态配置刷新

--- parent-demo/config-client-demo/src/main/java/com/mzyupc/config/controller/TestController.java
+++ parent-demo/config-client-demo/src/main/java/com/mzyupc/config/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.mzyupc.config.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2019/11/6 16:28:34
  */
 @RestController
+// 开启配置文件更新功能
+@RefreshScope
 public class TestController {
 
     @Value("${email}")
