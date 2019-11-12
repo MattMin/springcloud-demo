fcea4926d35ef7e7651fed8c78840bba2762e40a
MattMin <mzyupc@163.com>
2019-11-12 14:50:58 +0800

    添加注释

--- parent-demo/zookeeper-order-demo/src/main/java/com/mzyupc/zkorder/ZkOrderApp.java
+++ parent-demo/zookeeper-order-demo/src/main/java/com/mzyupc/zkorder/ZkOrderApp.java
@@ -10,6 +10,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @date 2019/11/12 10:56:54
  */
 @SpringBootApplication
+// 如果注册中心使用consul/zookeeper, @EnableDiscoveryClient可以向注册中心注册服务
 @EnableDiscoveryClient
 @EnableFeignClients
 public class ZkOrderApp {
